198f2ca5712bb36ae3e9479ef2f5ea4d70980355
Ajay Kandwal <kandwal.ajay@hotmail.com>
2016-03-12 18:17:32 +0530

    Initial commit for starting project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareThoughts::Application.config.secret_token = '571a1125b608c8f2702a53e34e0fbd74df8fab42efb2222fb5083f7ed1f911994e89b66706048e85fa00744da70c946dd51378f83492f318ce68bb57341d5bd3'
